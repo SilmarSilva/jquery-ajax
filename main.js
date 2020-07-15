5fbc0770e410eb43a078ba05b4a0838c299a10dd
STAR <sil.mar.luiz1@gmail.com>
2020-07-15 16:36:14 -0300

    feita aplicação da consulta do cep

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
 function consultaCep(){
-    
+    var cep = document.getElementById("cep").value;
+    var url = "https://viacep.com.br/ws/" + cep + "/json/";
+    console.log(url);
+    // o ($) é uma função que chama a biblioteca jquery
+    $.ajax({
+        url: url,
+        type: "GET",
+        success: function(response){
+            console.log(response);
+            // lembrando que o (#) é usado como seletor para id, enquanto que o (.) é seletor para class
+            $("#logradouro").html(response.logradouro); // forma alternativa para retornar os valores
+           // document.getElementById("logradouro").innerHTML = response.logradouro;
+            document.getElementById("bairro").innerHTML = response.bairro;
+            document.getElementById("localidade").innerHTML = response.localidade;
+            document.getElementById("uf").innerHTML = response.uf;
+        }
+    })
+
 }
